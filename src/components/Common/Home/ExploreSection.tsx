bb5e42de78f2242d75b76bdb4dd22d855277e373
Ajitsinh Rajput <asrrajput123ajit@gmail.com>
2025-08-04 22:46:48 -0400

    home update

--- src/components/Common/Home/ExploreSection.tsx
+++ src/components/Common/Home/ExploreSection.tsx
@@ -57,7 +57,6 @@ const ExploreSection = () => {
             )}
           </div>
 
-          {/* Keychains section */}
           <div className="col-md-4" data-aos="fade-left">
             {keychains && (
               <div className="d-flex flex-column align-items-center text-center h-100">
